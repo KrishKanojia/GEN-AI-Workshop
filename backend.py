d6924424b6e6377007a0aaf64fe60792b7a969cf
Krish Kanojia <75294518+KrishKanojia@users.noreply.github.com>
2024-11-19 23:32:35 +0500

    Api's completed

--- backend.py
+++ backend.py
@@ -1,78 +1,86 @@
-from typing import Union
-
-from fastapi import FastAPI, HTTPException, status, Query, Response
+from fastapi import FastAPI, HTTPException, status, Query, Response, Depends
+from typing import Annotated
 from pydantic import BaseModel
 from random import randrange
-
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlalchemy import func
 app = FastAPI()
 
 
-my_list = [
-    {"name": "Krish Kanojia", "level": 5, "marks": 99},
-    {"name": "Rahul Garcia", "level": 8, "marks": 11},
-]
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
-class Student(BaseModel):
-    name: str
-    level: int
+    
+    # id: int | None = Field(default=None, primary_key=True, sa_column_kwargs={"auto_increment": True})
+
+
+class Student(SQLModel, table=True):
+    name: str = Field(index=True, primary_key=True)
+    level: int | None = Field(default=0)
     marks: int
     
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread" : False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+# Create all table models
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+# Session to track changes in data
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/students")
-def get_all_students():
-    return {"data": my_list}
+def get_all_students(
+    session: SessionDep
+) -> list[Student]:
+    students = session.exec(select(Student)).all()
+    return students
 
 @app.post("/students", status_code=status.HTTP_201_CREATED)
-def create_student(student: Student):
-    student_dict = student.dict()
-    for item in my_list:
-        if student_dict["name"] in item["name"]:
-            return {"Message" : "Username Already Exist"}
-    my_list.append(student_dict)
-    print(student)
-    return {"data": student_dict}
+def create_student(student: Student, session: SessionDep):
+    session.add(student)
+    session.commit()
+    session.refresh(student)
+    return {"data": student}
 
 
 @app.get("/students/latest")
-def get_latest_student():
-    latest_student = my_list[-1]
-    return latest_student
+def get_latest_student(session: SessionDep) -> Student:
+    student = session.exec(select(Student).order_by(Student.name)).first()
+    return student
 
 @app.get("/student/{name}")
-def get_student_by_name(name: str):
-    student = find_student(name)
+def get_student_by_name(name: str, session: SessionDep) -> Student:
+    student = session.exec(select(Student).where(func.lower(Student.name) == name.lower())).first()
     print(f"student {student}")
     if not student:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Student with Name {name} not found")
-    return {"student_info" : student}
+    return student
 
 
 @app.delete("/student/{name}")
-def delete_student(name: str):
-    idx = student_index(name)
-    if idx == -1:
+def delete_student(name: str, session: SessionDep):
+    student = session.exec(select(Student).where(func.lower(Student.name) == name.lower())).first()
+    if not student:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Student with Name {name} is NOT DELETED")
-    my_list.pop(idx)
+    session.delete(student)
+    session.commit()
     return {"message" : f"Student {name} is DELETED SUCCESSFULLY"}
 
-def find_student(name: str):
-    temp_list = []
-    for item in my_list:
-        if name.lower() in item['name'].lower():
-            temp_list.append(item)
-    return temp_list
-
-
-def student_index(name: str):
-    for idx, item in enumerate(my_list):
-        print(f"{idx}")
-        if item["name"].lower() == name.lower():
-            return idx
-    return -1
 
